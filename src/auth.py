8fea0f1486ff82b942817d3d04e3194c4edb3a26
Tiago Berwanger <tiago.berwanger@mercos.com>
2024-08-11 23:27:00 -0300

    adiciona método de autenticação em APIs do google

---
+++ src/auth.py
@@ -0,0 +1,40 @@
+import os.path
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json
+SCOPES = [
+    'https://www.googleapis.com/auth/documents',
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive'
+]
+
+documento_id_com_dados_inquilinos = '1UzHWKA-qZTEfPDXs1l4o7FUp3BuI-MZcU3aEqYYJCbo'
+
+def get_authenticated_service(service_name: str, version: str):
+    """Shows basic usage of the API."""
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build(service_name, version, credentials=creds)
+        return service
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return None
