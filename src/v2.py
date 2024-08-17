2b2e3373dd170ed9ef33000f7f0ae76e98e5e981
Tiago Berwanger <tiago.berwanger@mercos.com>
2024-08-17 19:17:02 -0300

    adiciona no fluxo métodos para gerar pdf e enviar por e-mail

--- src/v2.py
+++ src/v2.py
@@ -1,8 +1,10 @@
 from src.auth import documento_id_com_dados_inquilinos
-from utils import abrir_planilha, formatar_dados, substituir_dados, alterar_status_contratos_gerados, obter_valores_de_planilha
+from utils import abrir_planilha, formatar_dados, substituir_dados, alterar_status_contratos_gerados, obter_valores_de_planilha, enviar_email_com_contrato, \
+    converter_contrato_para_pdf
 
 
 def main():
+    # abre planilha com dados de inquilinos
     planilha = abrir_planilha(chave=documento_id_com_dados_inquilinos)
     chave, valores = obter_valores_de_planilha(planilha)
 
@@ -12,6 +14,7 @@ def main():
         print('Todos os contratos já foram gerados!')
         return
 
+    # itera sobre valores de cada contrato
     for valor in valores:
         contrato_foi_gerado = valor[-1] == 'TRUE'
         if contrato_foi_gerado:
@@ -21,12 +24,20 @@ def main():
         for c, v in zip(chave, valor):
             dados_apresentados[c] = v
 
+        # coleta e formata dados
         dados_formatados = formatar_dados(dados_apresentados)
 
-        substituir_dados(dados_formatados)
+        # substitui dados formatados no contrato
+        documento_id = substituir_dados(dados_formatados)
 
-        alterar_status_contratos_gerados()
+        # transformar contrato em PDF
+        arquivo_em_bytes = converter_contrato_para_pdf(documento_id)
 
+        # envia rascunho por e-mail para eu conferir
+        enviar_email_com_contrato(dados_formatados.get('nome'), arquivo_em_bytes)
+
+    # altera status do contrato gerado
+    alterar_status_contratos_gerados()
 
 if __name__ == '__main__':
     main()
@@ -39,5 +50,7 @@ if __name__ == '__main__':
 # DONE Formatar CPF na saída, ajustar data por extenso e data do contrato
 # DONE Usar APIs do google (drive, sheets, docs)
 # DONE Ao salvar, mudar o status do contrato realizado
-# TODO Salvar, após concluído, esse contrato em formato PDF
-# TODO Enviar, após preenchido, esse contrato para o e-mail do inquilino
+# TODO Eliminar o gspread
+# TODO Salvar chaves no .env
+# DONE Salvar, após concluído, esse contrato em formato PDF
+# DONE Enviar, após preenchido, esse contrato para o meu e-mail para conferência
